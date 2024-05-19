0d2c8677e556ae806de772e799e4c6191bdfd218
raditha dissanayake <raditha@raditha.com>
2024-05-19 20:41:34 +0530

    A bit of JSX clean up

--- jsx/charts.jsx
+++ jsx/charts.jsx
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
 export function Chart() {
-    const [data, setData] = useState({author_commits: [], repo_commits: []});
+    const [data, setData] = useState({author_commits: [], repo_commits: [], project_commits: []});
+    const options = {
+        width: 600,
+        height: 600,
+        pieSliceText: 'value',
+        sliceVisibilityThreshold: 0.025,
+        chartArea: {
+            left: 100,
+            top: 75,
+            width: '100%',
+            height: '100%'
+        },
+        legend: {
+            alignment: 'center',
+            position: 'top'
+        }
+    };
 
+    /**
+     * This effect will fetch three different sets of data from the API and update the
+     * 'data' state with the results.
+     */
     useEffect(() => {
         fetch('/api/commits/author_commits/')
             .then(response => response.json())
@@ -11,8 +31,16 @@ export function Chart() {
         fetch('/api/commits/repository_commits/')
             .then(response => response.json())
             .then(json => setData({...data, repo_commits: json}));
+
+        fetch('/api/commits/project_commits/')
+            .then(response => response.json())
+            .then(json => setData({...data, project_commits: json}));
     }, []);
 
+    /**
+     * When a data fetch has completed this effect will be triggered and it will draw
+     * the charts and tables.
+     */
     useEffect(() => {
         if (window?.google?.charts) {
             window.google.charts.setOnLoadCallback(drawChart);
@@ -23,9 +51,11 @@ export function Chart() {
                 if(data?.author_commits?.length > 0) {
                     drawAuthorCommits(data.author_commits);
                 }
-                if(data?.repo_commits.length > 0) {
-                    console.log('should draw')
+                if(data?.repo_commits?.length > 0) {
                     drawRepositoryCommits(data.repo_commits)
+                }
+                if(data?.project_commits?.length > 0) {
+                    drawProjectCommits(data.project_commits)
                 }
             }
         }
@@ -40,23 +70,6 @@ export function Chart() {
         dataGoogle.addRows(data.map((item) => [item.repository.name, item.total, 
             item.repository.contributor, item.repository.lines]));
 
-        const options = {
-            width: 600,
-            height: 600,
-            pieSliceText: 'value',
-            sliceVisibilityThreshold: 0.025,
-            chartArea: {
-                left: 100,
-                top: 75,
-                width: '100%',
-                height: '100%'
-            },
-            legend: {
-                alignment: 'center',
-                position: 'top'
-            }
-        };
-
         const chart = new window.google.visualization.PieChart(document.getElementById('repo_chart_div'));
         chart.draw(dataGoogle, options);
 
@@ -70,27 +83,26 @@ export function Chart() {
         dataGoogle.addColumn('number', 'Commits');
         dataGoogle.addRows(data.map((item) => [item.author.name, item.total]));
 
-        const options = {
-            width: 600,
-            height: 600,
-            pieSliceText: 'value',
-            sliceVisibilityThreshold: 0.025,
-            chartArea: {
-                left: 100,
-                top: 75,
-                width: '100%',
-                height: '100%'
-            },
-            legend: {
-                alignment: 'center',
-                position: 'top'
-            }
-        };
-
         const chart = new window.google.visualization.PieChart(document.getElementById('author_chart_div'));
         chart.draw(dataGoogle, options);
 
         const table = new window.google.visualization.Table(document.getElementById('author_table_div'));
+        table.draw(dataGoogle, { showRowNumber: true, width: '100%', height: '600px' });
+    }
+
+    function drawProjectCommits(data) {
+        const dataGoogle = new window.google.visualization.DataTable();
+        dataGoogle.addColumn('string', 'Project');
+        dataGoogle.addColumn('number', 'Commits');
+        dataGoogle.addColumn('number', 'Active Repositories');
+        dataGoogle.addColumn('number', 'Total Lines of code');
+        dataGoogle.addRows(data.map((item) => [item.project.name, item.total, 
+            0, item.project.lines]));
+
+        const chart = new window.google.visualization.PieChart(document.getElementById('project_chart_div'));
+        chart.draw(dataGoogle, options);
+
+        const table = new window.google.visualization.Table(document.getElementById('project_table_div'));
         table.draw(dataGoogle, { showRowNumber: true, width: '100%', height: '600px' });
     }
 
@@ -109,6 +121,13 @@ export function Chart() {
             <div className='row mt-5'>
                 <div id="repo_chart_div" className='col-6'></div>
                 <div id="repo_table_div" className='col-6'></div>
+            </div>
+            <div className='row mt-5'>
+                <h2>Activity by Project</h2>
+            </div>
+            <div className='row mt-5'>
+                <div id="project_table_div" className='col-6'></div>
+                <div id="project_chart_div" className='col-6'></div>
             </div>
         </>)
 }
